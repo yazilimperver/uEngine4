64457512da52345ad307214c2184a1b8d25bddb2
yazilimperver <yazilimperver@gmail.com>
2022-09-26 21:07:20 +0300

    Linux icin yapilan bir takim guncellemeler

---
+++ code/project/vs2022/Project1/Project1/Source.cpp
@@ -0,0 +1,6 @@
+import <iostream>;
+
+int main() {
+	std::cout << "Merhaba dunya!\n";
+	return 0;
+}
--- code/src/test/basic_engine/asset/asset_loader_test.cpp
+++ code/src/test/basic_engine/asset/asset_loader_test.cpp
@@ -6,7 +6,7 @@
 #include "sdl_asset/sdl_texture_loader.h"
 
 #include "SDL.h"
-#include "SDL_Image.h"
+#include "SDL_image.h"
 
 using namespace basic_engine;
 
